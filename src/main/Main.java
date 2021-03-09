416100eda1462238c615e151e95650f4cd970546
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2021-03-09 16:27:24 -0300

    Adicionando testes da classe ProgressaoAritmetica na classe Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,16 @@
 package main;
 
+import herença.*;
+
 public class Main {
 	public static void main(String[] args) {
-	
+		Progressao progressao;
+		System.out.println("Progressão Aritimética com razão padrão:");
+		progressao = new ProgressaoAritmetica();
+		progressao.printProgressao(10);
+		System.out.println("Progressão Aritimética com razão 5:");
+		progressao = new ProgressaoAritmetica(5);
+		progressao.printProgressao(10);
 	}
 
 }
