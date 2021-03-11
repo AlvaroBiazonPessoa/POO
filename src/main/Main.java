0491a9d8f500e8364afdfa687b21bb5c079135cd
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2021-03-11 13:54:32 -0300

    Adicionado testes da classe ProgressaoGeeometrica na classe Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,6 +11,13 @@ public class Main {
 		System.out.println("Progressão Aritimética com razão 5:");
 		progressao = new ProgressaoAritmetica(5);
 		progressao.printProgressao(10);
+		
+		System.out.println("Progressaão Geométrica com razão padrão:");
+		progressao = new ProgressaoGeometrica();
+		progressao.printProgressao(10);
+		System.out.println("Progressão Geométrica com razão 3");
+		progressao = new ProgressaoGeometrica(3);
+		progressao.printProgressao(10);
 	}
 
 }
