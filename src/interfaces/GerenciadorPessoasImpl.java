877588ca4975836ce603adfc038f16228a737a8f
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2021-03-17 14:30:30 -0300

    Adicionando classe GerenciadorPessoasImpl ao pacote interfaces

---
+++ src/interfaces/GerenciadorPessoasImpl.java
@@ -0,0 +1,19 @@
+//GerenciadorPessoasImpl implementa a interface GerenciadorPessoas
+package interfaces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GerenciadorPessoasImpl implements GerenciadorPessoas {
+	private List<Pessoa> pessoas = new ArrayList<Pessoa>();
+	@Override
+	public List<Pessoa> obeterPessoa() {
+		return pessoas;
+	}
+
+	@Override
+	public void adicionarPessoa(Pessoa novaPessoa) {
+		pessoas.add(novaPessoa);
+	}
+
+}
